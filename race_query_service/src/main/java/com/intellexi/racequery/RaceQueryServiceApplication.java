d7fde148fbadc1c85581f2c055c707f351fb8b9e
Bruno Taras <bruno.taras@sigit.hr>
2024-05-21 21:46:21 +0200

    First commit

---
+++ race_query_service/src/main/java/com/intellexi/racequery/RaceQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.intellexi.racequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceQueryServiceApplication.class, args);
+	}
+
+}
---
+++ race_query_service/src/test/java/com/intellexi/racequery/RaceQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellexi.racequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
